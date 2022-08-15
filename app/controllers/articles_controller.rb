7b68e44c4b7d1da8d7cf2959f932e954e1f828c7
alijm0806 <alijm1104@gmail.com>
2022-08-15 16:58:27 -0500

    change key_api

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&from=2022-07-15&sortBy=publishedAt&apiKey=#{Rails.application.credentials.key_api}&language=en")
+    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&from=2022-07-15&sortBy=publishedAt&apiKey=#{Rails.application.credentials.news_key_api}&language=en")
     render json: response.as_json
   end
 end
